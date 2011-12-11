6e659f205e90376b9a2378f457c0f560b5fbacb9
Gabe Hart <gabe.hart@numerica.us>
2011-12-11 11:40:09 -0700

    Proto for content pages (Welcome page)

---
+++ Welcome/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0025)http://cs.unc.edu/~hartg/ -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!-- Get CSS -->
+<style media="screen" type="text/css">/*<![CDATA[*/@import 'gabriola.css';/*]]>*/</style>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<style type="text/css">#title,#glyphs p{font-family:"Gabriola"}</style>
+
+<!-- Set Up PHP Functions ------------------------------------------------- -->
+<?php
+function createMiniTile($img, $link, $text, $opacity, $name, $w, $h) {
+  $out='<div class="image">';
+  $out=$out.'<img src='.$img.' alt="" name="'.$name.'" style="opacity: 1;" />';
+  $out=$out.'<span class="overlay">
+             <a href='.$link.'>
+             <table width="'.$w.'" style="height: '.$h.'px; opacity: 1; "
+             onmouseover="'.$name.'.style.opacity=0.'.$opacity.';'.$name.'.filters.alpha.opacity='.$opacity.'"
+             onmouseout="'.$name.'.style.opacity=1;'.$name.'.filters.alpha.opacity=100">
+             <tr valign="middle"><td align="center">
+             <p class="linkText" style="text-align:center;">'.$text.'
+             </p>
+             </td></tr></table>
+             </a>
+             </span>';
+  $out=$out.'</div>';
+  return $out;
+}
+?>
+
+<title>Rebekkah and Gabe - Welcome</title>
+
+</head>
+
+<body bgcolor="White">
+<!-- Main Table------------------------------------------------------------ -->
+<center>
+<table width="704" cellpadding="0" cellspacing="0" border="0">
+<tbody>
+
+<tr>
+<!-- Left Col -->
+<td>
+  <img src="./images/Image.png">
+</td>
+
+<!-- Body Col -->     <!-- FIXME -->
+<td rowspan="3">
+  <table width="467" cellpadding="0" cellspacing="0" border="0">
+  <tbody>
+  <tr><td>
+    <img src="./images/top.png" height="10">
+  </td></tr>
+  <tr><td>
+    <img src="./images/body.png">
+  </td></tr>
+  <tr><td>
+    <img src="./images/bottom.png" height="10">
+  </td></tr>
+  </tbody>
+  </table>
+</td>
+</tr>
+
+<!-- Mini Panel -->
+<tr>
+<td>
+
+  <table cellpadding="0" cellspacing="0" border="0">
+  <tbody>
+  <tr>
+    <td><?php echo createMiniTile("./images/p11.png", ".", "Welcome", 85, "mp11", 117, 78) ?></td>
+    <td><?php echo createMiniTile("./images/p12.png", "./ThePlace/", "The Place", 85, "mp12", 118, 78) ?></td>
+  </tr>
+  <tr>
+    <td><?php echo createMiniTile("./images/p21.png", "./Registry/", "Registry", 85, "mp21", 117, 78) ?></td>
+    <td><?php echo createMiniTile("./images/p22.png", "./RSVP/", "RSVP", 85, "mp22", 118, 78) ?></td>
+  </tr>
+  <tr>
+    <td><?php echo createMiniTile("./images/p31.png", ".", "Welcome", 85, "mp31", 117, 77) ?></td>
+    <td><?php echo createMiniTile("./images/p32.png", "./ThePlace/", "The Place", 85, "mp32", 118, 77) ?></td>
+  </tr>
+  </tbody>
+  </table
+</td>
+</tr>
+
+<!-- Empty Expander Row -->
+<tr><td></td></tr>
+
+</tbody>
+
+</tbody></table>
+</center>
+
+</body></html>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" type="text/css" href="./css/style.css">
 <style type="text/css">#title,#glyphs p{font-family:"Gabriola"}</style>
 
-<!-- Set Up PHP Functions-------------------------------------------------- -->
+<!-- Set Up PHP Functions ------------------------------------------------- -->
 <?php
 
 /* Function to create a tile in the home screen */
@@ -44,22 +44,22 @@ function createTile($img, $link, $text, $opacity, $name) {
 <!-- First Row -->
 <tr>
 <td><img src="./images/p11.png" alt="" /></td>
-<td><?php echo "" . createTile("./images/p12.png", "www.test.com", "Welcome", 85, "p12") ?></td>
-<td><?php echo "" . createTile("./images/p13.png", "www.test.com", "The Place", 85, "p13") ?></td>
+<td><?php echo createTile("./images/p12.png", "./Welcome/", "Welcome", 85, "p12") ?></td>
+<td><?php echo createTile("./images/p13.png", "www.test.com", "The Place", 85, "p13") ?></td>
 </tr>
 
 <!-- Second Row -->
 <tr>
 <td><img src="./images/p21.png" alt="" /></td>
-<td><?php echo "" . createTile("./images/p22.png", "www.test.com", "RSVP", 85, "p22") ?></td>
-<td><?php echo "" . createTile("./images/p23.png", "www.test.com", "Registry", 85, "p23") ?></td>
+<td><?php echo createTile("./images/p22.png", "www.test.com", "RSVP", 85, "p22") ?></td>
+<td><?php echo createTile("./images/p23.png", "www.test.com", "Registry", 85, "p23") ?></td>
 </tr>
 
 <!-- Third Row -->
 <tr>
 <td><img src="./images/p31.png" alt="" /></td>
-<td><?php echo "" . createTile("./images/p32.png", "www.test.com", "The Place", 85, "p32") ?></td>
-<td><?php echo "" . createTile("./images/p33.png", "www.test.com", "RSVP", 85, "p33") ?></td>
+<td><?php echo createTile("./images/p32.png", "www.test.com", "The Place", 85, "p32") ?></td>
+<td><?php echo createTile("./images/p33.png", "www.test.com", "RSVP", 85, "p33") ?></td>
 </tr>
 
 </tbody>
