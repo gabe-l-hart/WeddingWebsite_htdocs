547e581e65e96a8b6d0fca8510940cd1b5f27ece
Gabe Hart <gabe.hart@numerica.us>
2012-03-02 22:23:20 -0700

    Photos without reload

--- Photos/index.php
+++ Photos/index.php
@@ -10,24 +10,10 @@ require './scripts/photos.php';
   <link rel="stylesheet" type="text/css" href="./css/photos.css">
   <script type="text/javascript" src="./scripts/jquery.ad-gallery.js"></script>
 
-  <script type="text/javascript">
-  $(function() {
-    var galleries = $('.ad-gallery').adGallery();
-    galleries[0].settings.use_description = false;
-  });
-  </script>
-
   <?php
     $userSet = new UserSet('rebekkah.gabe@googlemail.com');
     $userSet->populateAlbums();
     $userSet->displayAlbums();
-
-    // Show a specific album in the overlay
-    if (isset($_POST['album_id']))
-    {
-      $userSet->showAlbum($_POST['album_id']);
-    }
-
   ?>
 
   <!-- Make sure floated content gets expanded for -->
--- Photos/scripts/photos.php
+++ Photos/scripts/photos.php
@@ -85,7 +85,7 @@ class Album
   {
     return '
     <div class="albumTileDiv commonText">
-      <div class="albumTile itemTile" onclick="javascript: submit_show_album_'.$this->gphotoID.'()">
+      <div class="albumTile itemTile" onclick="javascript: show_overlay_'.$this->gphotoID.'()">
         <div class="overlayDiv">
           <div class="albumThumbnail">
             <img src="'.$this->thumbnail[0]->getURL().'">
@@ -96,12 +96,108 @@ class Album
       <form method=\'post\' action=\'\' name=\'show_album_'.$this->gphotoID.'\'>
         <input type=\'hidden\' name=\'album_id\' value=\''.$this->gphotoID.'\' />
       </form>
-    </div>
-    <script type=\'text/javascript\'>
-      function submit_show_album_'.$this->gphotoID.'() {
-        document.show_album_'.$this->gphotoID.'.submit();
+    </div>';
+  }
+
+  /* Create the scripts to show this album in the overlay */
+  function createShowScripts()
+  {
+    $out = '
+      <script>
+      var $overlay_wrapper_'.$this->gphotoID.';
+      var $overlay_panel_'.$this->gphotoID.';
+      var $gallery_'.$this->gphotoID.';
+
+      function show_overlay_'.$this->gphotoID.'() {
+          if ( !$overlay_wrapper_'.$this->gphotoID.' ) append_overlay_'.$this->gphotoID.'();
+          $overlay_wrapper_'.$this->gphotoID.'.fadeIn(700);
+          $overlay_panel_'.$this->gphotoID.'.fadeIn(0);
+          if ( !$gallery_'.$this->gphotoID.' ) create_gallery_'.$this->gphotoID.'();
+          fixOverlayHeight();
       }
-    </script>';
+
+      function create_gallery_'.$this->gphotoID.'() {
+        $gallery_'.$this->gphotoID.' = $(\'.ad-gallery\').adGallery();
+        $gallery_'.$this->gphotoID.'[0].settings.use_description = false;
+      }
+
+      function hide_overlay_'.$this->gphotoID.'() {
+          $overlay_wrapper_'.$this->gphotoID.'.fadeOut(500);
+      }
+
+      function append_overlay_'.$this->gphotoID.'() {
+          $overlay_wrapper_'.$this->gphotoID.' = $("<div class=\'overlay\' id=\'overlayBG\'></div>").appendTo( $("BODY") );
+          $overlay_panel_'.$this->gphotoID.' = $("<div class=\'photoOverlayPanel\' id=\'overlayPanel\'>\
+            <div id=\'overlayBG\'>\
+              <div class=\'photoOverlayPanelTop\'></div>\
+              <div class=\'photoOverlayPanelBody\'>\
+                <div class=\'overlayExit\'>\
+                  <a href=\'#\' class=\'hide-overlay_'.$this->gphotoID.' \'><img src=\'../images/closePanel.png\'></a>\
+                </div>\
+                <div class=\'galleryWrapper\'>\
+\
+\
+    <div id=\'gallery\' class=\'ad-gallery\'>\
+      <div class=\'ad-image-wrapper\'>\
+      </div>\
+      <div class=\'ad-controls\'>\
+      </div>\
+      <div class=\'ad-nav\'>\
+        <div class=\'ad-thumbs\'>\
+          <ul class=\'ad-thumb-list\'>';
+
+    foreach($this->photos as $photo)
+    {
+      $out .= '<li>\
+            <a href=\''.$photo->full_image[0]->getUrl().'\'>\
+              <img height=\'50px\' src=\''.$photo->thumbnail[0]->getUrl().'\' class=\''.$photo->gphotoID.'\'>\
+            </a>\
+          </li>';
+    }
+
+    $out .='</ul>\
+        </div>\
+      </div>\
+    </div>\
+\
+\
+              </div>\
+              </div>\
+              <div class=\'photoOverlayPanelBottom\'></div>\
+            </div>\
+          </div>").appendTo( $overlay_wrapper_'.$this->gphotoID.' );
+          attach_overlay_events_'.$this->gphotoID.'();
+      }
+
+      function attach_overlay_events_'.$this->gphotoID.'() {
+          $("A.hide-overlay_'.$this->gphotoID.'", $overlay_wrapper_'.$this->gphotoID.').click( function(ev) {
+              ev.preventDefault();
+              hide_overlay_'.$this->gphotoID.'();
+          });
+      }
+
+      $(function() {
+          $("A.show-overlay_'.$this->gphotoID.'").click( function(ev) {
+              ev.preventDefault();
+              show_overlay_'.$this->gphotoID.'();
+              fixOverlayHeight();
+          });
+      });
+
+      // Fix the overlay background height
+      function fixOverlayHeight() {
+        var bodyH = $(document).height();
+        var overlayH = $("#overlayPanel").height();
+        if (bodyH > overlayH) {
+          document.getElementById("overlayBG").style.height = bodyH + "px";
+        } else {
+          document.getElementById("overlayBG").style.height = overlayH + "px";
+        }
+      }
+      </script>';
+
+    echo $out;
+
   }
 }
 
@@ -133,16 +229,12 @@ class UserSet
       if ($entry instanceof Zend_Gdata_Photos_AlbumEntry)
       {
         $id = $entry->getGphotoId();
-        $this->albums["$id"] = new Album($entry->getTitle(), $id,
-                                         $entry->getMediaGroup()->getThumbnail());
+        $album = new Album($entry->getTitle(), $id,
+                           $entry->getMediaGroup()->getThumbnail());
+        $album->populatePhotos($this->client, $this->user);
+        $this->albums["$id"] = $album;
       }
     }
-  }
-
-  /* Populate the content of a given album */
-  function fetchAlbumContents($id)
-  {
-    $this->albums[$id]->populatePhotos($this->client, $this->user);
   }
 
   /* Get a handle to a specific album */
@@ -157,108 +249,11 @@ class UserSet
     foreach ($this->albums as $id => $album)
     {
       echo $album->createTile();
+      echo $album->createShowScripts();
     }
   }
 
-  /* Show a single album in the overlay */
-  function showAlbum($id)
-  {
-    $this->fetchAlbumContents($id);
-    $album = $this->getAlbum($id);
-    $out = '
-      <script>
-      var $overlay_wrapper;
-      var $overlay_panel;
 
-      function show_overlay() {
-          if ( !$overlay_wrapper ) append_overlay();
-          $overlay_wrapper.fadeIn(700);
-          $overlay_panel.fadeIn(0);
-          fixOverlayHeight();
-      }
-
-      function hide_overlay() {
-          $overlay_wrapper.fadeOut(500);
-      }
-
-      function append_overlay() {
-          $overlay_wrapper = $("<div class=\'overlay\' id=\'overlayBG\'></div>").appendTo( $("BODY") );
-          $overlay_panel = $("<div class=\'photoOverlayPanel\' id=\'overlayPanel\'>\
-            <div id=\'overlayBG\'>\
-              <div class=\'photoOverlayPanelTop\'></div>\
-              <div class=\'photoOverlayPanelBody\'>\
-                <div class=\'overlayExit\'>\
-                  <a href=\'#\' class=\'hide-overlay \'><img src=\'../images/closePanel.png\'></a>\
-                </div>\
-                <div class=\'galleryWrapper\'>\
-\
-\
-    <div id=\'gallery\' class=\'ad-gallery\'>\
-      <div class=\'ad-image-wrapper\'>\
-      </div>\
-      <div class=\'ad-controls\'>\
-      </div>\
-      <div class=\'ad-nav\'>\
-        <div class=\'ad-thumbs\'>\
-          <ul class=\'ad-thumb-list\'>';
-
-    foreach($album->photos as $photo)
-    {
-      $out .= '<li>\
-            <a href=\''.$photo->full_image[0]->getUrl().'\'>\
-              <img height=\'50px\' src=\''.$photo->thumbnail[0]->getUrl().'\' class=\''.$photo->gphotoID.'\'>\
-            </a>\
-          </li>';
-    }
-
-    $out .='</ul>\
-        </div>\
-      </div>\
-    </div>\
-\
-\
-              </div>\
-              </div>\
-              <div class=\'photoOverlayPanelBottom\'></div>\
-            </div>\
-          </div>").appendTo( $overlay_wrapper );
-          attach_overlay_events();
-      }
-
-      function attach_overlay_events() {
-          $("A.hide-overlay", $overlay_wrapper).click( function(ev) {
-              ev.preventDefault();
-              hide_overlay();
-          });
-      }
-
-      $(function() {
-          $("A.show-overlay").click( function(ev) {
-              ev.preventDefault();
-              show_overlay();
-              fixOverlayHeight();
-          });
-      });
-
-      // Fix the overlay background height
-      function fixOverlayHeight() {
-        var bodyH = $(document).height();
-        var overlayH = $("#overlayPanel").height();
-        if (bodyH > overlayH) {
-          document.getElementById("overlayBG").style.height = bodyH + "px";
-        } else {
-          document.getElementById("overlayBG").style.height = overlayH + "px";
-        }
-      }
-
-      // This will only get setup when reloading from opening an album
-      show_overlay();
-
-      </script>';
-
-    echo $out;
-
-  }
 
 }
 ?>
