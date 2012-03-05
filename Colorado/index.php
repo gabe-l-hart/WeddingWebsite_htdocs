27b900a4e98907a4c9aa44663d6480f24e1e61b4
Gabe Hart <gabe.hart@numerica.us>
2012-03-04 21:44:13 -0700

    Colorado page

--- Colorado/index.php
+++ Colorado/index.php
@@ -3,67 +3,169 @@
 <?php pageHeader("..", "Colorado"); ?>
 <?php subPageTop(); ?>
 
-
-  <p class="subPageTitle">Things to do in Colorado</p>
-
-  <!--Float the content to avoid being squashed to the left-->
-  <div style="float:left;">
-
   <style type="text/css">
   .coloradoCaption {
     font-size:24px;
+    font-weight:bold;
+    text-align:center;
+  }
+  .activityBullet a {
+    text-decoration:none;
+    color:#CFCFCF;
+    padding: 0px 0px 2px 0px;
+    border-bottom:solid 1px;
+  }
+  .coloradoDescription a {
+    text-decoration:none;
+    color:#CFCFCF;
+    padding: 0px 0px 2px 0px;
+    border-bottom:solid 1px;
   }
   </style>
 
-  <!--Surrounding Area-->
-  <p class="coloradoCaption">Surrounding Area</p>
-  <ul class="activityList">
-    <li class="activityBullet">Hiking</li>
-    <li class="activityBullet">Horse-Back Riding</li>
-    <li class="activityBullet">White-Water Rafting</li>
-    <li class="activityBullet">Camping</li>
-    <li class="activityBullet">Fishing</li>
-  </ul>
+  <p class="subPageTitle">Things to do in Colorado</p>
 
-  <!--Denver-->
-  <p class="coloradoCaption">Denver</p>
-  <ul class="activityList">
-    <li class="activityBullet">Colorado Rockies Game</li>
-    <li class="activityBullet">Denver Zoo</li>
-    <li class="activityBullet">Performing Arts Center</li>
-    <li class="activityBullet">Denver Art Museum</li>
-    <li class="activityBullet">Favorite Restaurants:</li>
-      <ul class="activityList">
-        <li class="activityBullet">Cafe Bar</li>
-        <li class="activityBullet">Root Down</li>
-        <li class="activityBullet">Park Burger</li>
-        <li class="activityBullet">Sushi Den</li>
-        <li class="activityBullet">Jelly (brunch)</li>
-        <li class="activityBullet">Snooze (brunch)</li>
-      </ul>
-  </ul>
+  <p class="coloradoDescription">For those of you looking to make a longer vacation out of your trip, here are just a few highlights of what Colorado has to offer.  For more information, the <a href="http://ranchretreats.com/tripplanning.html#local">Red Rock Ranch</a> offers a good list of activities in the local area.  The local chamber of commerce sites offer lots of information about visiting <a href="http://www.denverchamber.org/Page/visiting">Denver</a> and <a href="http://www.coloradospringschamber.org/visitor/things.asp">Colorado Springs</a>.  Also, some good information about Western Colorado can be found <a href="http://www.western-colorado-travel-secrets.com/">here</a>.</p>
 
-  <!--End the floated div so the rest will end up properly to the left-->
+  <!--Westcliffe Area-->
+  <div style="float:left; width:250px">
+    <p class="coloradoCaption">Westcliffe Area</p>
+    <ul class="activityList">
+      <li class="activityBullet">Hiking (
+        <a class="commonText" href="http://www.custercountyco.com/hiking.htm">more info</a>)
+        <ul>
+          <li class="activityBullet">Rainbow Trail.</li>
+          <li class="activityBullet">Sangre de Cristo Mountains</li>
+          <li class="activityBullet">Wet Mountains</li>
+        </ul>
+      </li>
+      <li class="activityBullet">Horse-Back Riding</li>
+        <ul>
+          <li class="activityBullet"><a href="http://www.bearbasinpacktrips.com/">Bear Basin Ranch</a></li>
+        </ul>
+      <li class="activityBullet">White-Water Rafting</li>
+        <ul>
+          <li class="activityBullet"><a href="http://www.arkansasrivertours.com/">Arkansas River Tours</a></li>
+          <li class="activityBullet"><a href="http://www.vfhrafting.com/">Vessels For Honor</a></li>
+        </ul>
+      <li class="activityBullet">Camping (
+        <a href="http://www.custercountyco.com/outdoors.htm">more info</a>)
+      </li>
+    </ul>
   </div>
-
-  <!--Clear to expand for floated content-->
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/Rainbow-trail-.jpg" width="125px">
+  </div>
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/tripplanning_horseback_lg.jpg" width="125px">
+  </div>
   <div style="clear:both;"></div>
 
+  <!--Denver-->
   <div style="float:left;">
-
-  <!--DEBUG-->
-  <p class="coloradoCaption">Surrounding Area</p>
-  <ul class="activityList">
-    <li class="activityBullet">Hiking</li>
-    <li class="activityBullet">Horse-Back Riding</li>
-    <li class="activityBullet">White-Water Rafting</li>
-    <li class="activityBullet">Camping</li>
-    <li class="activityBullet">Fishing</li>
-  </ul>
-
+    <p class="coloradoCaption">Denver</p>
+    <ul class="activityList">
+      <li class="activityBullet">Colorado Rockies (
+        <a href="http://colorado.rockies.mlb.com/">more info</a>)
+      </li>
+      <li class="activityBullet">Denver Zoo (
+        <a href="http://www.denverzoo.org/">more info</a>)
+      </li>
+      <li class="activityBullet">Performing Arts Center (
+        <a href="http://www.denvercenter.org/home.aspx">more info</a>)
+      </li>
+      <li class="activityBullet">Denver Art Museum (
+        <a href="http://www.denverartmuseum.org/home">more info</a>)
+      </li>
+      <li class="activityBullet">Favorite Restaurants:</li>
+        <ul class="activityList">
+          <li class="activityBullet">
+            <a href="http://www.cafebarcolorado.com/">Cafe Bar</a>
+          </li>
+          <li class="activityBullet">
+            <a href="http://www.rootdowndenver.com/">Root Down</a>
+          </li>
+          <li class="activityBullet">
+            <a href="http://parkburger.com/">Park Burger</a>
+          </li>
+          <li class="activityBullet">
+            <a href="http://sushiden.net/">Sushi Den</a>
+          </li>
+          <li class="activityBullet">
+            <a href="http://eatmorejelly.com/">Jelly</a> (brunch)
+          </li>
+          <li class="activityBullet">
+            <a href="http://www.snoozeeatery.com/">Snooze</a> (brunch)
+          </li>
+        </ul>
+    </ul>
   </div>
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/coors_field.jpg" width="300px">
+  </div>
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/Denver-CO-photo.jpg" width="300px">
+  </div>
+  <div style="clear:both;"></div>
 
-  <!--Clear to expand for floated content-->
+
+  <!--Colorado Springs-->
+  <div style="float:right; text-align:left; width:350px;">
+    <p class="coloradoCaption">Colorado Springs</p>
+    <ul class="activityList">
+      <li class="activityBullet">Pikes Peak
+        <ul class="activityList">
+          <li class="activityBullet">
+            <a href="http://cograilway.com/">Cog Railway</a>
+          </li>
+          <li class="activityBullet">
+            <a href="http://pikespeak.us.com/Activities/drive-to-the-summit.html">Drive to the summit</a>
+          </li>
+        </ul>
+      </li>
+      <li class="activityBullet">Garden of the Gods (
+        <a href="http://gardenofgods.com/home/index.cfm?flash=1">more info</a>)
+      </li>
+      <li class="activityBullet">Air Force Acadamy (
+        <a href="http://www.usafa.af.mil/information/visitors/index.asp">more info</a>)
+      </li>
+    </ul>
+  </div>
+  <div class="imageFrame" style="float:left; margin:10px 30px;">
+    <img src="./images/CogRailway.jpg" width="250px">
+  </div>
+  <div class="imageFrame" style="float:left; margin:10px 30px;">
+    <img src="./images/Garden-of-the-Gods.jpeg" width="250px">
+  </div>
+  <div style="clear:both;"></div>
+
+  <!--Western Colorado-->
+  <div style="float:left; text-align:left; width:350px;">
+    <p class="coloradoCaption">Western Colorado</p>
+    <ul class="activityList">
+      <li class="activityBullet">Mesa Verde National Park (
+        <a href="http://www.nps.gov/meve/index.htm">more info</a>)
+      </li>
+      <li class="activityBullet">San Jaun Mountains (
+        <a href="http://www.fs.usda.gov/sanjuan">more info</a>)
+      </li>
+      <li class="activityBullet">Black Canyon (
+        <a href="http://www.nps.gov/blca/index.htm">more info</a>)
+      </li>
+      <li class="activityBullet">Wine Tours (
+        <a href="http://www.western-colorado-travel-secrets.com/colorado-wineries.html">more info</a>)
+      </li>
+      <li class="activityBullet">Hot Springs (
+        <a href="http://www.western-colorado-travel-secrets.com/colorado-hot-springs.html">more info</a>)
+      </li>
+    </ul>
+  </div>
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/mesa_verde.JPG" width="250px">
+  </div>
+  <div class="imageFrame" style="float:right; margin:10px 30px;">
+    <img src="./images/sneffelsAspen.jpg" width="250px">
+  </div>
   <div style="clear:both;"></div>
 
 <?php subPageBottom(); ?>
