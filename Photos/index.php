d17bbc69d4503c845cba14a50e2743a6b61d70e3
Gabe Hart <gabe.l.hart@gmail.com>
2012-03-16 21:31:51 -0600

    Complete Photos

--- Photos/index.php
+++ Photos/index.php
@@ -10,6 +10,13 @@ require './scripts/photos.php';
   <link rel="stylesheet" type="text/css" href="./css/photos.css">
   <script type="text/javascript" src="./scripts/jquery.ad-gallery.js"></script>
 
+	<p class="subPageTitle">Photos</p>
+
+	<div class="descriptionText">
+		<p>We will upload our official wedding pictures here after the event.  You will also receive instructions at the wedding on how to upload your own pictures to the website so all guests can view and download pictures others have taken.</p>
+		<p>For now, please enjoy the below pictures of the Red Rock Ranch and our engagement photos!</p>
+	</div>
+
   <?php
     $userSet = new UserSet('rebekkah.gabe@googlemail.com');
     $userSet->populateAlbums();
