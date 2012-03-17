ed4c47f41286441f09003956fa4cc7c6c13303b5
Gabe Hart <gabe.l.hart@gmail.com>
2012-03-17 16:38:16 -0600

    Finish Welcome page

--- Welcome/index.php
+++ Welcome/index.php
@@ -6,10 +6,36 @@
 	<p class="subPageTitle">Our Wedding Weekend</p>
 
 	<div class="descriptionText">
-		<p>Welcome to our wedding website!  We could not be more excited to share with you our vision for the wedding.  We hope you will explore the information provided here and join us August 9<sup>th</sup>-12<sup>th</sup> for a celebration of love, friendship, and all that is to come.</p>
-		<p>We feel there is no better way to celebrate the exciting new phase we will be entering than surrounded by those who helped us get there.  As we have envisioned our wedding, our dream has been to spend days rather than hours with those we love.  Having lived in Michigan, Massachusetts, Minnesota, North Carolina, and Colorado, we want to gather those who have most supported us as individuals and as a couple along the way, drawing upon that love and support as we commit ourselves to one another and the life we will share.</p>
-		<p>In this spirit, we hope you will join us for our wedding weekend in Westcliffe, Colorado, beginning Thursday, August 9<sup>th</sup> and concluding Sunday, August 12<sup>th</sup>.  Housing will be provided for all guests at the Red Rock Ranch, a private estate nestled deep in the mountains.  After days of hiking, biking, and lounging on the beautiful grounds, take part in our formal ceremony and reception on Saturday, August 11<sup>th</sup>.</p>
-		<p>Please explore the rest of our website to learn more about the Red Rock Ranch, the specifics of the weekend, and how to RSVP.  We can not wait for the event to arrive, and we sincerely hope you can be part of this special time.</p>
+		<p>
+			Welcome to our wedding website!  We could not be more excited to share with you our vision for the wedding.
+			We hope you will explore the information provided here and join us August 9<sup>th</sup>-12<sup>th</sup> for
+			a celebration of love, friendship, and all that is to come.
+		</p>
+		<div class="imageFrame" style="float:right;">
+			<img src="./images/im1.jpg" alt="" width="200" >
+		</div>
+		<p>
+			We feel there is no better way to celebrate the exciting new phase we will be entering than surrounded by
+			those who helped us get there.  As we have envisioned our wedding, our dream has been to spend days rather
+			than hours with those we love.  Having lived in Michigan, Massachusetts, Minnesota, North Carolina, and
+			Colorado, we want to gather those who have most supported us as individuals and as a couple along the way,
+			drawing upon that love and support as we commit ourselves to one another and the life we will share.
+		</p>
+		<div class="imageFrame" style="float:left;">
+			<img src="./images/rrr.png" alt="" width="100" >
+		</div>
+		<p>
+			In this spirit, we hope you will join us for our wedding weekend in Westcliffe, Colorado, beginning Thursday,
+			August 9<sup>th</sup> and concluding Sunday, August 12<sup>th</sup>.  Housing will be provided for all guests
+			at the Red Rock Ranch, a private estate nestled deep in the mountains.  After days of hiking, biking, and
+			lounging on the beautiful grounds, take part in our formal ceremony and reception on Saturday, August
+			11<sup>th</sup>.
+		</p>
+		<p>
+			Please explore the rest of our website to learn more about the Red Rock Ranch, the specifics of the weekend,
+			and how to RSVP.  We can not wait for the event to arrive, and we sincerely hope you can be part of this
+			special time.
+		</p>
 	</div>
 
 <?php subPageBottom();?>
