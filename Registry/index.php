91d9d503aed6eb1e54af914881b2476fd6ed4556
Gabe Hart <gabe.l.hart@gmail.com>
2012-03-16 21:48:43 -0600

    Finish Registry with text

--- Registry/index.php
+++ Registry/index.php
@@ -29,10 +29,14 @@
   <!-- Title -->
   <p class="subPageTitle">Registry</p>
 
-  <!-- Description Text -->
-  <div style="margin:10px;">
-  <p>This is some really nifty descriptive text about how the registry and honeymoon are going to work.  Yay Cost Rica!!!</p>
-  </div>
+	<div class="descriptionText">
+		<p>When thinking about the type of gift that would be most meaningful and memorable around our wedding, we could think of nothing greater than help making our dream honeymoon come true.  After years of valuing travel as individuals and as a couple, we could not be more excited to explore Costa Rica together!</p>
+		<p>We have planned our ideal itinerary and have compiled a list of adventures.  Below please find places we would most love to stay, eat, and explore!</p>
+		<p>Please note that the below system is secured through Paypal.  Purchases will be deposited into a special honeymoon account in our name.</p>
+	</div>
+
+	<!-- Add spacing to bump the first row down -->
+	<br><br><br><br>
 
   <!-- Item Grid -->
   <?php
