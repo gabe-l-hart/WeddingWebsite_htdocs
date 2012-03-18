5a788a6a6bbb87080c4a5fd9971e9c729cbc8f1b
Gabe Hart <gabe.hart@numerica.us>
2012-03-17 21:13:36 -0600

    Remove registry spacers

--- Registry/index.php
+++ Registry/index.php
@@ -29,14 +29,11 @@
   <!-- Title -->
   <p class="subPageTitle">Registry</p>
 
-	<div class="descriptionText">
-		<p>When thinking about the type of gift that would be most meaningful and memorable around our wedding, we could think of nothing greater than help making our dream honeymoon come true.  After years of valuing travel as individuals and as a couple, we could not be more excited to explore Costa Rica together!</p>
-		<p>We have planned our ideal itinerary and have compiled a list of adventures.  Below please find places we would most love to stay, eat, and explore!</p>
-		<p>Please note that the below system is secured through Paypal.  Purchases will be deposited into a special honeymoon account in our name.</p>
-	</div>
-
-	<!-- Add spacing to bump the first row down -->
-	<br><br><br><br>
+  <div class="descriptionText">
+    <p>When thinking about the type of gift that would be most meaningful and memorable around our wedding, we could think of nothing greater than help making our dream honeymoon come true.  After years of valuing travel as individuals and as a couple, we could not be more excited to explore Costa Rica together!</p>
+    <p>We have planned our ideal itinerary and have compiled a list of adventures.  Below please find places we would most love to stay, eat, and explore!</p>
+    <p>Please note that the below system is secured through Paypal.  Purchases will be deposited into a special honeymoon account in our name.</p>
+  </div>
 
   <!-- Item Grid -->
   <?php
