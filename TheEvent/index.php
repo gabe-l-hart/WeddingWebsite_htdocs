059b76291285d9ede195e60b227ae198500cdd3e
Gabe Hart <gabe.l.hart@gmail.com>
2012-03-17 18:25:08 -0600

    Work on logistics

--- TheEvent/index.php
+++ TheEvent/index.php
@@ -3,6 +3,131 @@
 <?php pageHeader("..", "The Event"); ?>
 <?php subPageTop(); ?>
 
-  	<p class="subPageTitle">Logistics of the Weekend</p>
+<style type="text/css">
+
+.sectionCaption {
+	font-size: 20px;
+	text-decoration: underline;
+}
+
+.address {
+	font-style: italic;
+	text-align: center;
+	padding:0px;
+	margin:5px;
+}
+
+.logisticsBody a {
+  text-decoration:underline;
+  color:#CFCFCF;
+}
+
+</style>
+
+<!-- Title -->
+<p class="subPageTitle">Logistics of the Weekend</p>
+
+<div class="logisticsBody">
+
+	<p class="descriptionText">
+		As with any trip, juggling logistics can be one of the trickiest parts.
+		Our goal with this section is to cover some of the important questions
+		that will come up as you prepare for the weekend. As part of staying at
+		the Red Rock Ranch, every guest will be mailed a welcome packet which
+		will contain further details on many logistical questions as well as a
+		liability release form.
+	</p>
+
+	<!-- Arriving -->
+	<p class="sectionCaption">Arriving at the Red Rock Ranch</p>
+	<div class="imageFrame" style="float:right;">
+		<img src="./images/coMap.png" alt="" width="250" >
+	</div>
+	<p class="descriptionText" style="padding:0px; margin:0px;">
+		The Red Rock Ranch is located at:
+	</p>
+	<div class="descriptionText address">
+		525 Brush Hollow Road<br>
+		Westcliffe, CO 81252
+	</div>
+	<p class="descriptionText" style="padding:0px; margin:0px;">
+		Guests may arrive Thursday anytime after 3pm and must depart the ranch
+		on Sunday by 11am. Detailed directions to the Red Rock Ranch can be found
+		<a href="http://ranchretreats.com/tripplanning.html#directions">here</a>
+		on their website.
+	</p><br>
+
+	<!-- Flying -->
+	<p class="sectionCaption">Flying to the Red Rock Ranch</p>
+	<p class="descriptionText">
+		The Red Rock Ranch is located in Westcliffe, Colorado, approximately
+		three hours from Denver and one and a half hours from Colorado Springs.
+		Flights can be booked into the Denver airport or the Colorado Springs
+		airport.  Flights into Denver tend to be slightly cheaper, but driving
+		to the Ranch from the Colorado Springs airport is more convenient.
+		Feel free to choose whichever option works best for you!
+	</p>
+	<p class="descriptionText">
+		While you will need a car to get from the airport to the Red Rock Ranch,
+		we hope to avoid all guests feeling they need to rent a car.  After
+		receiving your RSVP, we will send you the contact information for other
+		guests whose flights arrive at a similar time.  If you would like to
+		share a rental car with others, feel free to contact these guests and
+		arrange to carpool.
+	</p>
+
+	<!-- Lodging and Meals -->
+	<div class="imageFrame" style="float:right;">
+		<img src="./images/rrr.png" alt="" width="100" >
+	</div>
+	<p class="sectionCaption">Lodging and Meals</p>
+	<p class="descriptionText">
+		The Red Rock Ranch consists of three houses, all located together on a
+		private estate.  The rooms vary in style and setup, but we will make
+		every effort to maximize both space and comfort in organizing the
+		specifics of lodging.  Please note that most bathrooms are shared among
+		several rooms. 
+	</p>
+	<p class="descriptionText">
+		All meals will be provided at the Red Rock Ranch.  Guests are welcome to
+		help themselves to food throughout the day and dinners will be cooked
+		communally.
+	</p>
+
+	<!-- Children -->
+	<p class="sectionCaption">Children</p>
+	<p class="descriptionText">
+		Please note that babysitting will be provided during the ceremony.
+		While kids are welcome throughout the weekend, we ask that families
+		with small children utilize this childcare during the ceremony.
+	</p>
+
+	<!-- Suggested Packing Items -->
+	<p class="sectionCaption">Suggested Packing Items</p>
+	<p class="descriptionText">
+		In packing for the trip, consider including the following items:
+		<ul>
+			<li>Swimsuit for the pool and Jacuzzi </li>
+			<li>
+				Warm clothing for evenings; the mountain air is cool, even in
+				the summer!
+			</li>
+			<li>Clothes for outdoor activities, including footwear</li>
+			<li>
+				Shoes and clothing to exercise, if interested in using the on-site
+				fitness facility
+			</li>
+			<li>
+				Camera; guests will have the ability after the wedding to post
+				their pictures on this website!
+			</li>
+			<li>Sunscreen; remember it is easier to get burned at high altitudes!
+			</li>
+		</ul>
+		*Note: While dress throughout the weekend will be casual and
+		comfortable, the ceremony and reception on Saturday night will be formal.
+	</p>
+
+</div>
 
 <?php subPageBottom(); ?>
