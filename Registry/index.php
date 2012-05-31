bc5f3273f80ec6357e1eb637ea6767abacb71e0c
Gabe Hart <gabe.l.hart@gmail.com>
2012-05-30 21:16:02 -0600

    Add registry redirect note

--- Registry/index.php
+++ Registry/index.php
@@ -33,6 +33,9 @@
     <p>When thinking about the type of gift that would be most meaningful and memorable around our wedding, we could think of nothing greater than help making our dream honeymoon come true.  After years of valuing travel as individuals and as a couple, we could not be more excited to explore Costa Rica together!</p>
     <p>We have planned our ideal itinerary and have compiled a list of adventures.  Below please find places we would most love to stay, eat, and explore!</p>
     <p>Please note that the below system is secured through Paypal.  Purchases will be deposited into a special honeymoon account in our name.</p>
+
+    <p><b>NOTE</b>: Once you have completed your Paypal transaction, please wait for the page to redirect you back to this site so the database can update.  You will see a confirmation message pop up when you return.</p>
+
   </div>
 
   <!-- Item Grid -->
